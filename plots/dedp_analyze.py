47ef7328435d18908b53b492eae6ce58c45bf8ca
Shivesh Pathak <sapatha2@illinois.edu>
2019-11-14 15:25:59 -0600

    figure 3 nearly done !

--- plots/dedp_analyze.py
+++ plots/dedp_analyze.py
@@ -18,5 +18,38 @@ def analyze_hdf5(hdf_file):
     cutoffs = np.sort(cutoffs)
     df = pd.DataFrame({'cutoff':cutoffs, 'dEdp': dEdp_mu, 'err': dEdp_std})
     df.to_json('dedp_vmc.json')
+    return df
 if __name__ == '__main__':
-    analyze_hdf5('dedp_vmc.hdf5')
+    #analyze_hdf5('dedp_vmc.hdf5')
+    import matplotlib.pyplot as plt 
+    import statsmodels.api as sm
+    from statsmodels.sandbox.regression.predstd import wls_prediction_std
+    from numpy.polynomial import polynomial 
+
+    df = pd.read_json('dedp_vmc.json')
+    
+    x = df['cutoff']
+    y = df['dEdp']
+    err = df['err']
+
+    err = err[x>=1e-2]
+    y = y[x>=1e-2]
+    x = x[x>=1e-2]
+    
+
+    ols = sm.WLS(y, sm.add_constant(x[:, np.newaxis]**3),weight=1/err**3)
+    ols = ols.fit()
+
+    prstd, iv_l, iv_u = wls_prediction_std(ols)
+    #Fit cubic to each 
+    p_l = polynomial.polyfit(x, iv_l, [0,3])
+    p_u = polynomial.polyfit(x, iv_u, [0,3])
+
+    fig, ax = plt.subplots(1, 1, figsize=(3,3))
+    ax.errorbar(x, y, yerr=err,fmt='ko')
+    x = np.linspace(0,max(x),100)
+    ax.fill_between(x, p_l[0] + p_l[3]* x**3, p_u[0] + p_u[3]*x**3, color='gray', alpha=0.5)
+    ax.errorbar(x[0], ols.params[0], yerr = (p_l[0] - p_u[0])/2, fmt='s', c='g')
+    ax.set_ylabel(r'$\partial E/\partial p$ (Ha)')
+    ax.set_xlabel(r'$\epsilon$ (Bohr)')
+    plt.savefig('dedp.pdf',bbox_inches='tight')
