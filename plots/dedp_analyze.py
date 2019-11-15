23cfd23ad2637da2526a09cb5506d1b7e3bea7f7
Shivesh Pathak <sapatha2@illinois.edu>
2019-11-14 22:37:09 -0600

    newer analysis file

--- plots/dedp_analyze.py
+++ plots/dedp_analyze.py
@@ -1,80 +1,99 @@
 import numpy as np 
 import pandas as pd
 import h5py
+import matplotlib.pyplot as plt
+from pyqmc.reblock import _reblock, reblock_summary, opt_block, optimally_reblocked
 
-def bootstrap(dpH, e, dppsi, N):
-    dEdps = []
-    for i in range(N):
-        sample = np.random.choice(dpH.shape[0], dpH.shape[0], replace=True)
-        dpH_sample = dpH[sample]
-        e[sample] = e[sample]
-        dppsi[sample] = dppsi[sample]
-        
-        dEdp = np.mean(dpH_sample, axis=0) -\
-               np.mean(e, axis=0) * np.mean(dppsi, axis=0)[:, np.newaxis]
-        dEdps.append(dEdp)
-    dEdps = np.array(dEdps)
-    dEdp_mu = np.mean(dEdps, axis=0)
-    dEdp_std = np.std(dEdps, axis=0)
-    return dEdp_mu[0], dEdp_std[0]
-
-def analyze_hdf5(hdf_file, nsplit, nbootstrap, end=20000):
+def analyze_hdf5(hdf_file):
     with h5py.File(hdf_file, 'r') as hdf:
-        dpH = np.array([np.array(x) for x in list(hdf['pgraddpH'])])
-        dppsi = np.array([np.array(x) for x in list(hdf['pgraddppsi'])])
         e = np.array([np.array(x) for x in list(hdf['pgradtotal'])])
-    
-    print(dpH.shape)
-    warmup = 100
-    dpH = dpH[warmup:end]
-    dppsi = dppsi[warmup:end]
-    e = e[warmup:end]
-    dpH = np.array(np.split(dpH, nsplit)).mean(axis=1)
-    dppsi = np.array(np.split(dppsi, nsplit)).mean(axis=1)
-    e = np.array(np.split(e, nsplit)).mean(axis=1)
-    
-    dEdp_mu, dEdp_std = bootstrap(dpH, e, dppsi, nbootstrap)
-
-    cutoffs = list(np.logspace(-8, -1, 20)) + list([0.05,0.075, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-    cutoffs = np.sort(cutoffs)
-    df = pd.DataFrame({'cutoff':cutoffs, 'dEdp': dEdp_mu, 'err': dEdp_std})
-    df.to_json('dedp_vmc.json')
-    print(df)
-    return df
+        dppsi = np.array([np.array(x) for x in list(hdf['pgraddppsi'])])
+        dpH = np.array([np.array(x) for x in list(hdf['pgraddpH'])])
+    return e, dppsi, dpH
 
 if __name__ == '__main__':
-    analyze_hdf5('dedp_vmc.hdf5', 100, 100, end=14000)
-    '''
-    import matplotlib.pyplot as plt 
-    import statsmodels.api as sm
-    from statsmodels.sandbox.regression.predstd import wls_prediction_std
-    from numpy.polynomial import polynomial 
-
-    df = pd.read_json('dedp_vmc.json')
+    e, dppsi, dpH = analyze_hdf5('dedp_vmc.hdf5')
     
-    x = df['cutoff']
-    y = df['dEdp']
-    err = df['err']
-
-    err = err[x>=1e-2]
-    y = y[x>=1e-2]
-    x = x[x>=1e-2]
-    
-
-    ols = sm.WLS(y, sm.add_constant(x[:, np.newaxis]**3),weight=1/err**3)
-    ols = ols.fit()
-
-    prstd, iv_l, iv_u = wls_prediction_std(ols)
-    #Fit cubic to each 
-    p_l = polynomial.polyfit(x, iv_l, [0,3])
-    p_u = polynomial.polyfit(x, iv_u, [0,3])
-
-    fig, ax = plt.subplots(1, 1, figsize=(3,3))
-    ax.errorbar(x, y, yerr=err,fmt='ko')
-    x = np.linspace(0,max(x),100)
-    ax.fill_between(x, p_l[0] + p_l[3]* x**3, p_u[0] + p_u[3]*x**3, color='gray', alpha=0.5)
-    ax.errorbar(x[0], ols.params[0], yerr = (p_l[0] - p_u[0])/2, fmt='s', c='g')
-    ax.set_ylabel(r'$\partial E/\partial p$ (Ha)')
-    ax.set_xlabel(r'$\epsilon$ (Bohr)')
-    plt.savefig('dedp.pdf',bbox_inches='tight')
+    #Check timetraces for warmups
+    dppsi = dppsi[:, 0]
+    dpH = dpH[:, 0]
     '''
+    plt.plot(dpH[:, :25])
+    plt.show()
+    exit(0)
+    '''
+
+    #Plot histograms
+    warmup = 400
+    e = e[warmup:]
+    dppsi = dppsi[warmup:]
+    dpH = dpH[warmup:]
+    '''
+    plt.hist(dpH[:, 0])
+    plt.show()
+    exit(0)
+    '''
+
+    #Reblock each quantity
+    '''
+    df = pd.DataFrame({'e':dpH[:,0]})
+    nblocks = []
+    err = []
+    err_err = []
+    nblocks = np.array([4000, 1000, 500, 200, 100, 80, 50, 40, 20, 10, 5])
+    for nblock in nblocks:
+        d = reblock_summary(df, nblock)
+        err.append(d["standard error"])
+        err_err.append(d["standard error error"])
+    plt.errorbar(e.shape[0]/nblocks, err, err_err,fmt='o')
+    plt.xlabel('step per block')
+    plt.ylabel('error')
+    plt.show()
+    exit(0)
+    '''
+
+    #Evaluate errors after reblocking
+    steps_per_block = 50
+    reblocked_e = np.array(_reblock(e, e.shape[0]/steps_per_block))
+    reblocked_dppsi = np.array(_reblock(dppsi, dppsi.shape[0]/steps_per_block))
+    reblocked_dpH = np.array([np.array(_reblock(dpH[:,i], dpH.shape[0]/steps_per_block))
+                      for i in range(dpH.shape[1])]).T
+    
+    print("Reblocking")
+    for v in [reblocked_e, reblocked_dppsi, reblocked_dpH[:, 0]]:
+        print(v.mean(), v.std()/np.sqrt(v.shape[0]))
+
+    #Bootstrapping
+    Nbs = 400
+
+    print("Bootstrapping")
+    for v in [reblocked_e, reblocked_dppsi, reblocked_dpH[:, 0]]:
+        bs = []
+        for i in range(Nbs):
+            sample = np.random.choice(v.shape[0], v.shape[0], replace=True)
+            sampled_v = v[sample]
+            bs.append(sampled_v.mean())
+        bs = np.array(bs)
+        print(bs.mean(), bs.std())
+
+    #dEdp Bootstrap 
+    dEdp = []
+    for i in range(Nbs):
+        sample = np.random.choice(v.shape[0], v.shape[0], replace=True)
+        sampled_e = reblocked_e[sample]
+        sampled_dppsi = reblocked_dppsi[sample]
+        sampled_dpH = reblocked_dpH[sample]
+
+        bs_e = sampled_e.mean()
+        bs_dppsi = sampled_dppsi.mean()
+        bs_dpH = sampled_dpH.mean(axis=0)
+
+        dEdp.append(bs_dpH- np.array([bs_e * bs_dppsi] * bs_dpH.shape[0]))
+    dEdp = np.array(dEdp)
+
+    means = dEdp.mean(axis=0)
+    stds  = dEdp.std(axis=0)
+    cutoffs = list(np.logspace(-8, -1, 20)) + list([0.05,0.075, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+    cutoffs = np.sort(cutoffs)
+    plt.errorbar(cutoffs, means, stds, fmt='o')
+    plt.show()
